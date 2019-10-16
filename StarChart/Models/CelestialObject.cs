3feec1f545b7924205f67510453a3f5a99bea41e
Adam Pankowski <adam.pankowski@nmgroup.com>
2019-10-16 13:07:44 +0100

    Add Satellites Property

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,5 +15,8 @@ namespace StarChart.Models
         public string Name { get; set; }
 
         public int? OrbitedObjectId { get; set; }
+
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
     }
 }
