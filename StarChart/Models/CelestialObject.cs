be8006ebe43a42c4376b78c65da2c375d0885f00
Adam Pankowski <adam.pankowski@nmgroup.com>
2019-10-16 13:06:05 +0100

    Add Name Property

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,8 @@ namespace StarChart.Models
     public class CelestialObject
     {
         public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
     }
 }
